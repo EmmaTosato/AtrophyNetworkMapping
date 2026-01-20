c4d1d0fade4d38e58d99fcc3620eb59893b982a3
EmmaTosato <emmamariasole.tosato@studenti.unipd.it>
2026-01-20 14:46:06 +0100

    feat: implement AlexNet3D and compact config JSON

--- src/DL_analysis/cnn/models.py
+++ src/DL_analysis/cnn/models.py
@@ -1,4 +1,4 @@
-# models.py
+
 import torch
 import torch.nn as nn
 from torchvision.models.video import r3d_18
@@ -15,6 +15,42 @@ class ResNet3D(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+
+class AlexNet3D(nn.Module):
+    def __init__(self, num_classes=2, input_channels=1):
+        super(AlexNet3D, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv3d(input_channels, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            nn.Conv3d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            nn.Conv3d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool3d((6, 6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
 
 
 class DenseNet3D(nn.Module):
